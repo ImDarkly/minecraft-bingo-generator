341eb82e7742041bad1f64beb6cac2ecbdf9b69a
ImDarkly <svit.darkly@gmail.com>
2023-11-23 11:42:58 +0200

    Fixed link

--- app/card/page.tsx
+++ app/card/page.tsx
@@ -1,14 +1,14 @@
 "use client";
 import { BingoCardGrid } from "@/components/ui/bingo-card-grid";
 import items from "@/api/items.json";
-import * as React from 'react';
-import { useSearchParams } from 'next/navigation'
+import * as React from "react";
+import { useSearchParams } from "next/navigation";
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
 import { HomeIcon } from "@radix-ui/react-icons";
 
 export default function Home() {
-    const searchParams = useSearchParams()
+    const searchParams = useSearchParams();
 
     const rand = (min: number, max: number): number =>
         Math.floor(Math.random() * (max - min + 1)) + min;
@@ -16,18 +16,23 @@ export default function Home() {
     const cards: { id: number; name: string; title: string }[] = [];
 
     for (let i = 1; i < 26; i++) {
-        const card = searchParams.get('b' + i)
-        cards.push(items[card]);
-    }
-
+        const card = searchParams.get('b' + i);
+        if (card) {
+          cards.push(items[parseInt(card)]);
+        }
+      }
+      
     return (
         <div className="flex flex-col items-center justify-center h-full w-full gap-4">
             <h1 className="text-3xl">
                 Your <span className="text-rose-500">Bingo</span> card
             </h1>
             <BingoCardGrid cards={cards} />
-            <Link href={"/"}><Button size="icon" variant={"ghost"}><HomeIcon className="h-4 w-4"/></Button></Link>
+            <Link href={"/"}>
+                <Button size="icon" variant={"ghost"}>
+                    <HomeIcon className="h-4 w-4" />
+                </Button>
+            </Link>
         </div>
     );
 }
-
