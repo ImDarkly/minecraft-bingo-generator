dfe944388233bc44ec010a71725c634502b66253
Oleksandr Hryhorchuk <66973381+ImDarkly@users.noreply.github.com>
2023-12-14 19:12:45 +0200

    updated button visulas

--- app/page.tsx
+++ app/page.tsx
@@ -37,7 +37,7 @@ export default function Home() {
                 />
                 <BingoCardGrid cards={cards} />
                 <div className="flex items-center flex-col gap-2 w-full">
-                    <Button onClick={generateCards} className="w-full rounded-full">
+                    <Button onClick={generateCards} className="w-full rounded-full bg-red-400 active:scale-95">
                         Generate
                     </Button>
                     <Link
@@ -47,7 +47,7 @@ export default function Home() {
                         ).join("&")}`}
                         className="w-full"
                     >
-                        <Button variant={"outline"} className="w-full rounded-full">
+                        <Button variant={"outline"} className="w-full rounded-full active:scale-95">
                             Open
                         </Button>
                     </Link>
